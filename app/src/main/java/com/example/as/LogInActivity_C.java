927c74b47bd57212728c5f17129f539a0561a47f
AlejandraJimenezAvalos <55304400+AlejandraJimenezAvalos@users.noreply.github.com>
2020-10-24 20:43:39 -0500

    Rules user V1

--- app/src/main/java/com/example/as/LogInActivity_C.java
+++ app/src/main/java/com/example/as/LogInActivity_C.java
@@ -29,65 +29,69 @@ import static com.example.as.classes.ConstantsDataBase.USERS;
 
 public class LogInActivity_C extends AppCompatActivity {
 
-    EditText txt_correo, txt_pass;
-    Button btn_iniciar;
+    EditText textEmail, textPass;
+    Button buttonLogIn;
     private FirebaseAuth mAuth;
 
-    private List<UserData> userDataList = new ArrayList<>();
+    private final List<UserData> userDataList = new ArrayList<>();
 
     private FirebaseAuth.AuthStateListener authStateListener;
-    private DatabaseReference mDatabase;
 
-    private String Tipo;
+    private String type;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        txt_correo=findViewById(R.id.txt_user_name_login);
-        txt_pass=findViewById(R.id.txt_password_login);
-        btn_iniciar=findViewById(R.id.btn_iniciarsesion_login);
+
+        textEmail =findViewById(R.id.txt_user_name_login);
+        textPass =findViewById(R.id.txt_password_login);
+        buttonLogIn =findViewById(R.id.btn_iniciarsesion_login);
         mAuth=FirebaseAuth.getInstance();
-        btn_iniciar.setOnClickListener(v -> logIn() );
-        mDatabase= FirebaseDatabase.getInstance().getReference();
+        buttonLogIn.setOnClickListener(v -> logIn() );
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
 
         authStateListener = firebaseAuth -> {
-                FirebaseUser user;
-                user = mAuth.getCurrentUser();
-                if (null != user) {
-                    DatabaseReference reference = FirebaseDatabase.getInstance().getReference(USERS);
-                    reference.addValueEventListener(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(@NonNull DataSnapshot snapshot) {
-                            String type = "";
-                            List<String> keys = new ArrayList<>();
-                            userDataList.clear();
-                            for(DataSnapshot keyNode : snapshot.getChildren()){
-                                keys.add(keyNode.getKey());
-                                UserData userData = keyNode.getValue(UserData.class);
-                                userDataList.add(userData);
+            FirebaseUser user;
+            user = mAuth.getCurrentUser();
+            if (null != user) {
+                DatabaseReference reference = FirebaseDatabase.getInstance().getReference(USERS);
+                reference.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        String type;
+                        List<String> keys = new ArrayList<>();
+                        userDataList.clear();
+                        for(DataSnapshot keyNode : snapshot.getChildren()){
+                            keys.add(keyNode.getKey());
+                            UserData userData = keyNode.getValue(UserData.class);
+                            userDataList.add(userData);
 
-                                String email = userData.getEmail();
-                                Toast.makeText(LogInActivity_C.this, "Email 1 " + email, Toast.LENGTH_SHORT).show();
-                                String email2 = FirebaseAuth.getInstance().getCurrentUser().getEmail();
-                                if (email.equals(email2)) {
-                                    type = userData.getType();
-                                    if (type.equals(ADMIN)) {
-                                        startActivity(new Intent(LogInActivity_C.this, AdminMainActivity.class));
-                                    } else {
-                                        startActivity(new Intent(LogInActivity_C.this, UserMainActivity_C.class));
-                                    }
-                                    Toast.makeText(LogInActivity_C.this, "type: " + type, Toast.LENGTH_SHORT).show();
+                            String email = userData.getEmail();
+                            Toast.makeText(LogInActivity_C.this, "Email 1 " + email,
+                                    Toast.LENGTH_SHORT).show();
+                            String email2 = FirebaseAuth.getInstance().getCurrentUser().getEmail();
+                            if (email.equals(email2)) {
+                                type = userData.getType();
+                                if (type.equals(ADMIN)) {
+                                    startActivity(new Intent(LogInActivity_C.this,
+                                            AdminMainActivity.class));
+                                } else {
+                                    startActivity(new Intent(LogInActivity_C.this,
+                                            UserMainActivity_C.class));
                                 }
-
+                                Toast.makeText(LogInActivity_C.this, "type: " + type,
+                                        Toast.LENGTH_SHORT).show();
                             }
 
                         }
-                        @Override
-                        public void onCancelled(@NonNull DatabaseError error) {
-                        }
-                    });
-                }
+
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                    }
+                });
+            }
             };
     }
 
@@ -104,12 +108,14 @@ public class LogInActivity_C extends AppCompatActivity {
     }
 
     private void logIn () {
-        final String email;
-        final String passw;
-        email = txt_correo.getText().toString();
-        passw = txt_pass.getText().toString();
-        mAuth.signInWithEmailAndPassword(email, passw).addOnFailureListener(e ->
-                Toast.makeText(LogInActivity_C.this, e.getMessage(), Toast.LENGTH_SHORT).show());
+        String email = textEmail.getText().toString();
+        String passw = textPass.getText().toString();
+        if (email.isEmpty() && passw.isEmpty()) {
+            Toast.makeText(this, "Debe introducir la contraseña y el correo", Toast.LENGTH_SHORT).show();
+        } else {
+            mAuth.signInWithEmailAndPassword(email, passw).addOnFailureListener(e ->
+                    Toast.makeText(LogInActivity_C.this, e.getMessage(), Toast.LENGTH_SHORT).show());
+        }
     }
 
     public boolean onCreateOptionsMenu(Menu menu){
@@ -119,41 +125,16 @@ public class LogInActivity_C extends AppCompatActivity {
 
     public boolean onOptionsItemSelected(MenuItem item){
         int id = item.getItemId();
-
-        if (id== R.id.Item1){
-            //Toast.makeText(this, "Opcion 1: olvide la contraseña", Toast.LENGTH_SHORT).show();
-            mAuth.signOut();
-            Toast.makeText(this, "Seción cerrada", Toast.LENGTH_SHORT).show();
-        }
-        else if (id==R.id.Item2){
-            //Toast.makeText(this, "Opcion 2: algo más", Toast.LENGTH_SHORT).show();
-            Toast.makeText(this, "User: " + mAuth.getCurrentUser().getEmail(), Toast.LENGTH_SHORT).show();
+        if (id == R.id.Item1) {
+            Toast.makeText(this, "Opcion 1: olvide la contraseña", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.Item2) {
+            Toast.makeText(this, "Opcion 2: algo más", Toast.LENGTH_SHORT).show();
         }
         return super.onOptionsItemSelected(item);
     }
 
-    public void registro(View view){
+    public void logUp(View view){
         startActivity(new Intent(this, LogUpActivity.class));
-    }
-    private boolean check() {
-        try {
-            String cor, pas, confpass;
-            cor = txt_correo.getText().toString();
-            pas = txt_pass.getText().toString();
-
-            if (cor.isEmpty()) {
-
-                Toast.makeText(this, "Por favor ingrese un correo", Toast.LENGTH_SHORT).show();
-                return false;
-            } else if (pas.isEmpty()) {
-
-                Toast.makeText(this, "Por favor ingrese una contraseña", Toast.LENGTH_SHORT).show();
-                return false;
-            }
-        } catch (Exception e) {
-            Toast.makeText(this, "error:" + e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
-        }
-            return true;
     }
 
 }
